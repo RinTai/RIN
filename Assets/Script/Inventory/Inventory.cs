def22a8570b6c96dafeca234536a67725e42dd1a
Zukunft39 <119741340+Zukunft39@users.noreply.github.com>
2023-02-03 16:34:25 +0800

    先删了

--- Assets/Script/Inventory/Inventory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Inventory : MonoBehaviour
-{
-    public bool[] isFull;//表示是否储存满
-    public GameObject[] slots;//设为游戏物体（槽）
-}
