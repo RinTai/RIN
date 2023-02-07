b4c4bfd606cdebeaaf0003ea3cb18773361c5a43
woshiyuanfangdeni <117359194+woshiyuanfangdeni@users.noreply.github.com>
2023-02-07 15:58:12 +0800

    门的问题解决了（虽然动画有点丑）

--- Assets/Map/Map Script/openDoor.cs
+++ Assets/Map/Map Script/openDoor.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class openDoor : MonoBehaviour
 {
-    //Animator anim;
+    Animator anim;
     PlatformEffector2D door;
+    bool isOpen = false;
+    bool isPlayer = false;
     void Start()
     {
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         door= GetComponent<PlatformEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isPlayer)
         {
-            //anim.SetTrigger("open");
+            isOpen = true;
             door.surfaceArc = 0f;
         }
         else
         {
+            isOpen = false;
             door.surfaceArc = 360f;
+        }
+        if (isOpen)
+        {
+            anim.SetTrigger("open");
+        }
+        else
+        {
+            anim.SetTrigger("close");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            isPlayer= true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            isPlayer= false;
         }
     }
 }
