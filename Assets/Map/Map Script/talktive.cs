a4b030e68592be8051c9dbdb97d4f73dd2132b9b
woshiyuanfangdeni <117359194+woshiyuanfangdeni@users.noreply.github.com>
2023-02-24 15:10:13 +0800

    该改错

--- Assets/Map/Map Script/talktive.cs
+++ Assets/Map/Map Script/talktive.cs
@@ -26,7 +26,7 @@ public class talktive : MonoBehaviour
             Invoke("GetItems", waitTime);
         }
     }
-    private void OnTriggerEnter2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
